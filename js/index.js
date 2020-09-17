45a8d4831c30c2d943553f187610799b396e7152
rolandsfr <rolands.affaires@gmail.com>
2020-09-17 07:18:16 +0300

    desktop staff-apps v1

--- js/index.js
+++ js/index.js
@@ -1,106 +1,135 @@
-$(".preloader-img").addClass("fadeIn-custom");
-
-let bars = $(".burger-menu");
-
-bars.click(() => {
-    bars.toggleClass("toggleBars");
-    $("#header nav").toggleClass("slideOut");
-    $("header nav").toggleClass("slideIn")
-});
-
-$(window).on("load", () => {
-    setTimeout(() => {
-        $(".preloader").animate({opacity: 0}, 500, () => {
-            $(".preloader").css("display", "none");
-            $(".preloader-img").removeClass("rotate");
-            $("body").css("overflow", "visible");
-        })
+// $(window).on("load", () => {
+//     setTimeout(() => {
+//         $(".preloader").animate({opacity: 0}, 500, () => {
+//             $(".preloader").css("display", "none");
+//             $(".preloader-img").removeClass("rotate");
+//             $("body").css("overflow", "visible");
+//         })
         
 
-    }, 1500);
+//     }, 1500);
 
-    setTimeout(() => {
-        if($(window).width() >= 1024) {
-          new WOW().init();  
+//     setTimeout(() => {
+//         if($(window).width() >= 1024) {
+//           new WOW().init();  
+//         }
+//     }, 1300) 
+
+// });
+
+// (function(){
+//     emailjs.init("user_z3F7jyQ8qvbeOobnstw7p");
+// })();
+
+ // $("footer form").submit((e) => {
+ //    e.preventDefault();
+ //    $(".form-error").removeClass("success");
+
+ //    $("footer form input[type=text], footer textarea").css("border", "none")
+
+ //    let fieldsCompleted = false;
+
+ //    if($("footer form textarea").val() === ""){
+ //        $(".form-error").html("Please enter your message.")
+ //        $("footer textarea").css("border", "1px solid #DC000F");
+
+ //        fieldsCompleted = false;
+ //    } else {
+ //        fieldsCompleted = true;
+
+ //        for(let i = 0; i < $("footer form input").length; i++) {
+
+ //            if($("footer form input").eq(i).val() === "") {
+ //                $(".form-error").html("Please fill in all the fields.")
+ //                $("footer form input").eq(i).css("border", "1px solid #DC000F");
+
+ //                fieldsCompleted = false;
+ //            }
+ //        }
+ //    }
+
+//     if(fieldsCompleted) {
+//         $(".form-error").html("");
+
+//         let templateParams = {
+//             name: $("footer #name").val(),
+//             ign: "Not precised",
+//             email: $("footer #email").val(),
+//             topic: "Not precised",
+//             message: $("footer textarea").val()
+//         }
+
+//         emailjs.send('gmail', 'msg', templateParams)
+//             .then(function(response) {
+//                 $(".form-error").html("Your message was successfully sent!").addClass("success");
+//             }, function(error) {
+//                 $(".form-error").html("Something went wrong... Refresh page and try again.");
+//             });
+//     }     
+
+// })
+
+let parentNode = $(".helper-block");
+
+$("[data-block]").each((index, el) => {
+    if($(el).attr("data-block") !== $(".active-tab").attr("data-tab")) {
+        $(el).hide()
+    }
+})
+
+$(".tabs").on("click", ".tab", (e) => {
+    $("input[type=text], textarea").css("border", "none");
+    $(".tab").removeClass("active-tab");
+    $(e.target).addClass("active-tab");
+
+    // Reset error
+    $(".form-error").html("");
+
+    // let currentTab = $(".active-tab").attr("data-tab")
+
+    $("[data-block]").each((index, el) => {
+        if($(el).attr("data-block") !== $(".active-tab").attr("data-tab")) {
+            $(el).hide()
+        } else {
+            $(el).show()
+
+            // Specific range withing which inputs have to be found
+            parentNode = $(el);
         }
-    }, 1300) 
+    })
+})
 
-
-});
-
-// $("#wrapper").css("min-height", ($(window).height() + "px"));
-
-$(".header-static").css("height", $("header").outerHeight() + "px")
-normalizeViewport($("main"), $("header"));
-
-
-$(window).resize(() => {
-    // $("#wrapper").css("min-height", ($(window).height() + "px"));
-    $(".header-static").css("height", $("header").outerHeight() + "px")
-    normalizeViewport($("main"), $("header"));
-});
-
-$("a").each((index, el) => {
-    $(el).on("click", (event) => {
-        event.preventDefault();
-        $("body").css("overflow", "hidden");
-        $(".page-unloader").css("display", "block");
-        $(".page-unloader").animate({opacity: 1}, 400);
-
-        setTimeout(() => {
-            window.location.href = $(el).attr("href");
-        }, 500);
-    });
-});
-
-(function(){
-    emailjs.init("user_z3F7jyQ8qvbeOobnstw7p");
-})();
-
- $("footer form").submit((e) => {
+$("form").on("submit", (e) => {
     e.preventDefault();
-    $(".form-error").removeClass("success");
 
-    $("footer form input[type=text], footer textarea").css("border", "none")
+    // Show success message and hide error(red) borders
+    $(".form-error").html("Your application will be reviewed soon, We will reply as soon as possible.").addClass("success");
+    $("input[type=text], textarea").css("border", "none");
 
-    let fieldsCompleted = false;
-
-    if($("footer form textarea").val() === ""){
-        $(".form-error").html("Please enter your message.")
-        $("footer textarea").css("border", "1px solid #DC000F");
-
-        fieldsCompleted = false;
-    } else {
-        fieldsCompleted = true;
-
-        for(let i = 0; i < $("footer form input").length; i++) {
-
-            if($("footer form input").eq(i).val() === "") {
-                $(".form-error").html("Please fill in all the fields.")
-                $("footer form input").eq(i).css("border", "1px solid #DC000F");
-
-                fieldsCompleted = false;
-            }
+    // Checks whether all inputs and textareas within specific section are empty
+    for(let i = 0; i < parentNode.find("input[type=text]").length; i++) {
+        if(parentNode.find("input").eq(i).val() === "") {
+            $(".form-error").html("Please fill in all the fields.")
+            parentNode.find("input").eq(i).css("border", "1px solid #DC000F");
+            $(".form-error").removeClass("success");
         }
     }
 
-    if(fieldsCompleted) {
-        $(".form-error").html("");
-
-        let templateParams = {
-            name: $("footer #name").val(),
-            ign: "Not precised",
-            email: $("footer #email").val(),
-            topic: "Not precised",
-            message: $("footer textarea").val()
+    for(let i = 0; i < parentNode.find("textarea").length; i++) {
+        if(parentNode.find("textarea").eq(i).val() === "") {
+            $(".form-error").html("Please fill in all the fields.")
+            parentNode.find("textarea").eq(i).css("border", "1px solid #DC000F");
+            $(".form-error").removeClass("success");
         }
+    }
 
-        emailjs.send('gmail', 'msg', templateParams)
-            .then(function(response) {
-                $(".form-error").html("Your message was successfully sent!").addClass("success");
-            }, function(error) {
-                $(".form-error").html("Something went wrong... Refresh page and try again.");
-            });
-    }     
+    // The same container is used in each and every section
+    for(let i = 0; i < $(".basic-info").find("input[type=text]").length; i++) {
+        if($(".basic-info").find("input").eq(i).val() === "") {
+            $(".form-error").html("Please fill in all the fields.")
+            $(".basic-info").find("input").eq(i).css("border", "1px solid #DC000F");
+            $(".form-error").removeClass("success");
+        }
+    }
 
 })
